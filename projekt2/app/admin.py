cb1f97a44c579d3569f3438cfdba12d92d748d32
Dominik P <80773318+DominikP-github@users.noreply.github.com>
2024-01-26 22:40:50 +0100

    Delete projekt2 directory

--- projekt2/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Keyword,YourModel,SearchHistory
-
-admin.site.register(Keyword)
-
-admin.site.register(YourModel)
-
-admin.site.register(SearchHistory)
-# Register your models here.
